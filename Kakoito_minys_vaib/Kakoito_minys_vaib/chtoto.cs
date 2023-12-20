4227b123ccfb3bccad2f2d5fca46b183f7ecc776
sashka <144311184+iamsashka@users.noreply.github.com>
2023-12-20 10:19:22 +0300

    Update chtoto.cs

--- Kakoito_minys_vaib/Kakoito_minys_vaib/chtoto.cs
+++ Kakoito_minys_vaib/Kakoito_minys_vaib/chtoto.cs
@@ -13,3 +13,11 @@ internal interface jsonchiki
     static abstract List<Polzovatel>? Load();
     static abstract void Save(List<Polzovatel> users);
 }
+
+internal interface crudi
+{
+    void Create(List<Polzovatel>? users, jsonchiki json);
+    void Read(List<Polzovatel>? users, Polzovatel user, int posit);
+    void Update();
+    void Delete(List<Polzovatel>? users, int posit);
+}
